8ab7abdce90ee8e5c8a747a1021926665352154e
Harshil Sharaf <62461329+HarshilSharaf@users.noreply.github.com>
2021-04-06 15:39:45 +0530

    Updates

---
+++ precache-manifest.5e95eaef7b74e8e3e39a573551b9319d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe53f60b25e80bdc1a66293b0ae6d1f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "459f2e957d76a49bcc50",
+    "url": "/static/css/2.ffdfee16.chunk.css"
+  },
+  {
+    "revision": "319c69a0c7877150404a",
+    "url": "/static/css/4.bf7421d6.chunk.css"
+  },
+  {
+    "revision": "e502d19838c5255bbeb8",
+    "url": "/static/css/main.9e8fb266.chunk.css"
+  },
+  {
+    "revision": "459f2e957d76a49bcc50",
+    "url": "/static/js/2.6bd0dfc9.chunk.js"
+  },
+  {
+    "revision": "9d6c7ceac0b3e30640e10d7dcbcf64a3",
+    "url": "/static/js/2.6bd0dfc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "313f713d9447f9039463",
+    "url": "/static/js/3.26d9c71b.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/3.26d9c71b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "319c69a0c7877150404a",
+    "url": "/static/js/4.dba5d350.chunk.js"
+  },
+  {
+    "revision": "e502d19838c5255bbeb8",
+    "url": "/static/js/main.ea5a7c4a.chunk.js"
+  },
+  {
+    "revision": "06bab105c1681b145a60",
+    "url": "/static/js/runtime-main.b932ea5e.js"
+  },
+  {
+    "revision": "c9686b45ea1dfc0fa5f0817259ec59c1",
+    "url": "/static/media/avatar.c9686b45.jpeg"
+  },
+  {
+    "revision": "f971dd94f62c4662e732ea74d2561260",
+    "url": "/static/media/ayin_qoph.f971dd94.png"
+  },
+  {
+    "revision": "aea271b930d57b95ca626bb1a9fb27ee",
+    "url": "/static/media/contact.aea271b9.svg"
+  },
+  {
+    "revision": "075fe2c07825b280a4a6c902e7e06631",
+    "url": "/static/media/defaultAvatar.075fe2c0.svg"
+  },
+  {
+    "revision": "1976f27fa4b3f0b14b524d0f86f0aee2",
+    "url": "/static/media/dummy-project.1976f27f.png"
+  },
+  {
+    "revision": "9aece68254c95eca4a0875e213e1d521",
+    "url": "/static/media/focused_working.9aece682.svg"
+  }
+]);
